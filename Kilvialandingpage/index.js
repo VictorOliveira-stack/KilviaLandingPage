5ccb836f104f50437d567958f0a0b9ddc501c6bf
VictorOliveira-stack <victordevgit@gmail.com>
2025-01-30 18:37:49 -0300

    04

--- Kilvialandingpage/index.js
+++ Kilvialandingpage/index.js
@@ -3,3 +3,34 @@ function cta(){
     "_blank" )
 } 
 
+let currentIndex = 0
+const slides = document.querySelectorAll(".slide")
+const totalSlides = slides.length
+const nextButton = document.querySelector('.next')
+const prevButton = document.querySelector('.prev')
+
+//funçao relacionada com a variaveis acima; faz movimentar o slide
+
+function showSlide (index){
+    if( index < 0){
+        currentIndex = totalSlides -1;
+    }else if (index >= totalSlides) {
+        currentIndex = 0
+    }
+
+    const offset = currentIndex *100 //isso faz deslocar a imagem
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+nextButton.addEventListener('click', ()=>{
+    currentIndex++;
+    showSlide(currentIndex)
+})
+
+prevButton.addEventListener('click', () =>{
+      currentIndex--;
+      showSlide(currentIndex)
+})
+
+//inicia o carrossel
+showSlide(currentIndex)
